eb84ff7927ba51fc2cba92bbebbf5fc4f39a54e6
Ulrich Vollert <ulrivo@gmx.de>
2025-02-27 16:53:25 +0100

    example.extension to commuterflow.extension

--- src/register_types.cpp
+++ src/register_types.cpp
@@ -1,4 +1,5 @@
 #include "register_types.h"
+#include "gdexample.h"
 #include <gdextension_interface.h>
 #include <godot_cpp/core/class_db.hpp>
 #include <godot_cpp/core/defs.hpp>
@@ -10,7 +11,7 @@ void initialize_gdextension_types(ModuleInitializationLevel p_level) {
 	if (p_level != MODULE_INITIALIZATION_LEVEL_SCENE) {
 		return;
 	}
-	//GDREGISTER_CLASS(YourClass);
+	GDREGISTER_CLASS(GDExample);
 }
 
 void uninitialize_gdextension_types(ModuleInitializationLevel p_level) {
